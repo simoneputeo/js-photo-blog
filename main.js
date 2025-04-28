a9b7d642779a474109b22bfa5f90a2f9d223c6c6
simoneputeo <s.puteo@gmail.com>
2025-04-28 16:17:33 +0200

    logic done

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const apiUri = `https://jsonplaceholder.typicode.com/photos`;
+const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
+const cardsContainer = document.getElementById('cards-container');
 
-for(let i=0; i< 10; i++){
-axios
-.get(apiUri)
-.then(response=>{console.log(i, response.data.response)})
+axios.get(apiUri)
+  .then(response => {
+    const photos = response.data.slice(0, 10);
+    photos.forEach(photo => createCard(photo));
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+function createCard(photo) {
+  const col = document.createElement('div');
+  col.className = 'col-sm-10 col-md-5 col-lg-4 mb-4';
+
+  const card = `
+    <div class="card m-2 rounded-0">
+      <div class="card-body m-2 rounded-0 position-relative">
+        <div class="card-pin">
+          <img src="./img/pin.svg" alt="pin" width="40">
+        </div>
+        <img src="${photo.url}" class="card-img rounded-0 card-image" alt="${photo.title}">
+        <p class="card-text edu-tas-beginner mt-3">${photo.title}</p>
+        <p class="card-text sometype-mono mt-3">${photo.date}</p>
+
+    
+      </div>
+    </div>
+  `;
+
+  col.innerHTML = card;
+  cardsContainer.appendChild(col);
 }
